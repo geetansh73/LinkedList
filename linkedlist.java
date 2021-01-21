b5c3d7d70daabb1429ddce6c86f2790947d5be2e
Geetansh Gupta <67698473+geetansh73@users.noreply.github.com>
2021-01-21 19:29:29 +0530

    Add files via upload

---
+++ linkedlist.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class linkedlist
+{
+public static void main(String g[])
+{
+LinkedList <Integer>obj=new LinkedList<>();
+obj.add(1);
+obj.add(2);
+obj.add(3);
+obj.add(4);
+obj.add(5);
+System.out.println(obj);
+}
+}
